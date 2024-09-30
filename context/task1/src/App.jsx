7146e715600762b30428546dc9860257b0b3f1c2
Volodymyr <babihuk888@gmail.com>
2024-09-30 13:02:11 +0300

    redux task1

---
+++ context/task1/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { createContext, useState } from 'react';
+import ThemedButton from './ThemedButton';
+
+const themes = {
+  dark: {
+    fontColor: '#ffffff',
+    background: '#222222',
+  },
+  light: {
+    fontColor: '#000000',
+    background: '#eeeeee',
+  },
+};
+
+const App = () => {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <div>
+      <ThemeContext.Provider value={{ theme: themes[theme], toggleTheme }}>
+        <ThemedButton />
+      </ThemeContext.Provider>
+      <button onClick={toggleTheme} className="btn">
+        Default Theme
+      </button>
+    </div>
+  );
+};
+
+export default App;
+
+export const ThemeContext = createContext({
+  theme: themes.dark,
+  toggleTheme: () => {},
+});
