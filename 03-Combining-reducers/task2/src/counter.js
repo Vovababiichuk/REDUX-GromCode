0325c7fb79bcd98d9ae2f866dd2de82f9680428f
Volodymyr <babihuk888@gmail.com>
2024-10-10 16:54:40 +0300

    task2

--- 03-Combining-reducers/task2/src/counter.js
+++ 03-Combining-reducers/task2/src/counter.js
@@ -23,6 +23,19 @@ export const reset = () => {
   };
 };
 
+export const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    case RESET:
+      return 0;
+    default:
+      return state;
+  }
+};
+
 const store = createStore(
   counterReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
