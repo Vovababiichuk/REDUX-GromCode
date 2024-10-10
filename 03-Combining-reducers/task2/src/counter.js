ad773d7aff1220a1e5653df43ab17d0cc24f3c83
Volodymyr <babihuk888@gmail.com>
2024-10-10 16:56:44 +0300

    task2

--- 03-Combining-reducers/task2/src/counter.js
+++ 03-Combining-reducers/task2/src/counter.js
@@ -1,8 +1,7 @@
 const { createStore } = require('redux');
 
-export const INCREMENT = 'COUNTER/INCREMENT';
-export const DECREMENT = 'COUNTER/DECREMENT';
-export const RESET = 'COUNTER/RESET';
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
 
 export const increment = () => {
   return {
@@ -16,20 +15,12 @@ export const decrement = () => {
   };
 };
 
-export const reset = () => {
-  return {
-    type: RESET,
-  };
-};
-
 export const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
     case DECREMENT:
       return state - 1;
-    case RESET:
-      return 0;
     default:
       return state;
   }
