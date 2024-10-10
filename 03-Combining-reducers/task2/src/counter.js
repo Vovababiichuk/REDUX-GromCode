93af2c4a0a972190bff55587ff4bbd18b51dea18
Volodymyr <babihuk888@gmail.com>
2024-10-10 16:57:05 +0300

    task2

--- 03-Combining-reducers/task2/src/counter.js
+++ 03-Combining-reducers/task2/src/counter.js
@@ -15,7 +15,7 @@ export const decrement = () => {
   };
 };
 
-export const counterReducer = (state = 0, action) => {
+const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
