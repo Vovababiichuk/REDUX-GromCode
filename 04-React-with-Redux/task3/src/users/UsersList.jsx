de69573f19f311343650bd5dd64f7c59da947900
Volodymyr <babihuk888@gmail.com>
2024-10-15 21:20:00 +0300

    redux-4-task3

--- 04-React-with-Redux/task3/src/users/UsersList.jsx
+++ 04-React-with-Redux/task3/src/users/UsersList.jsx
@@ -1,49 +1,101 @@
+// import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { setCurrentPage } from './users.actions';
+// import User from './User';
+// import Pagination from './Pagination';
+
+// const UsersList = () => {
+//   const { usersList, currentPage } = useSelector(state => state);
+//   const dispatch = useDispatch();
+
+//   const itemsPerPage = 3;
+//   const totalItems = usersList.length;
+//   const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+//   const handleGoPrev = () => {
+//     if (currentPage > 0) {
+//       dispatch(setCurrentPage(currentPage - 1));
+//     }
+//   };
+
+//   const startIndex = currentPage * itemsPerPage;
+//   const endIndex = startIndex + itemsPerPage;
+//   const usersToDisplay = usersList.slice(startIndex, endIndex);
+
+//   const handleGoNext = () => {
+//     if (currentPage < totalPages - 1) {
+//       dispatch(setCurrentPage(currentPage + 1));
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <Pagination
+//         goPrev={handleGoPrev}
+//         goNext={handleGoNext}
+//         currentPage={currentPage + 1}
+//         totalItems={totalItems}
+//         itemsPerPage={itemsPerPage}
+//       />
+//       <ul className="users">
+//         {usersToDisplay.map(user => (
+//           <User key={user.id} name={user.name} age={user.age} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default UsersList;
+
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 import { setCurrentPage } from './users.actions';
 import User from './User';
 import Pagination from './Pagination';
 
-const UsersList = () => {
-  const { usersList, currentPage } = useSelector(state => state);
-  const dispatch = useDispatch();
-
-  const itemsPerPage = 3;
-  const totalItems = usersList.length;
-  const totalPages = Math.ceil(totalItems / itemsPerPage);
-
-  const handleGoPrev = () => {
-    if (currentPage > 0) {
-      dispatch(setCurrentPage(currentPage - 1));
-    }
-  };
-
+const UsersList = ({ users, currentPage, itemsPerPage, goPrev, goNext }) => {
   const startIndex = currentPage * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  const usersToDisplay = usersList.slice(startIndex, endIndex);
-
-  const handleGoNext = () => {
-    if (currentPage < totalPages - 1) {
-      dispatch(setCurrentPage(currentPage + 1));
-    }
-  };
+  const usersToDisplay = users.slice(startIndex, endIndex);
 
   return (
-    <div>
+    <ul className="users">
       <Pagination
-        goPrev={handleGoPrev}
-        goNext={handleGoNext}
+        goPrev={() => goPrev(currentPage)}
+        goNext={() => goNext(currentPage, users.length, itemsPerPage)}
         currentPage={currentPage + 1}
-        totalItems={totalItems}
+        totalItems={users.length}
         itemsPerPage={itemsPerPage}
       />
-      <ul className="users">
-        {usersToDisplay.map(user => (
-          <User key={user.id} name={user.name} age={user.age} />
-        ))}
-      </ul>
-    </div>
+      {usersToDisplay.map(user => (
+        <User key={user.id} name={user.name} age={user.age} />
+      ))}
+    </ul>
   );
 };
 
-export default UsersList;
+const mapStateToProps = state => {
+  return {
+    users: state.usersList,
+    currentPage: state.currentPage,
+    itemsPerPage: 3,
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  goPrev: currentPage => {
+    console.log('goPrev clicked, currentPage:', currentPage);
+    if (currentPage > 0) {
+      dispatch(setCurrentPage(currentPage - 1));
+    }
+  },
+  goNext: (currentPage, totalUsers, itemsPerPage) => {
+    const totalPages = Math.ceil(totalUsers / itemsPerPage);
+    if (currentPage < totalPages - 1) {
+      dispatch(setCurrentPage(currentPage + 1));
+    }
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersList);
