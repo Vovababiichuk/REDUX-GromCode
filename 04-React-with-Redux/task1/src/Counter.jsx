440fa3d2ad953b4c1bbe9c13ee5097ebbdeb8264
Volodymyr <babihuk888@gmail.com>
2024-10-15 17:28:52 +0300

    redux-4-task1

--- 04-React-with-Redux/task1/src/Counter.jsx
+++ 04-React-with-Redux/task1/src/Counter.jsx
@@ -1,25 +1,61 @@
+// import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { increment, decrement, reset } from './counter.actions';
+
+// const Counter = () => {
+//   const count = useSelector(state => state.counter);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="counter">
+//       <button className="counter__button" onClick={() => dispatch(decrement())}>
+//         -
+//       </button>
+//       <span className="counter__value" onClick={() => dispatch(reset())}>
+//         {' '}
+//         {count}{' '}
+//       </span>
+//       <button className="counter__button" onClick={() => dispatch(increment())}>
+//         +
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Counter;
+
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement, reset } from './counter.actions';
+import { connect } from 'react-redux';
+import * as counterActions from '../counter.actions';
 
-const Counter = () => {
-  const count = useSelector(state => state.counter);
-  const dispatch = useDispatch();
-
+const Counter = ({ counter, decrement, increment, reset }) => {
   return (
     <div className="counter">
-      <button className="counter__button" onClick={() => dispatch(decrement())}>
+      <button className="counter__button" onClick={decrement}>
         -
       </button>
-      <span className="counter__value" onClick={() => dispatch(reset())}>
-        {' '}
-        {count}{' '}
+      <span className="counter__value" onClick={reset}>
+        {counter}
       </span>
-      <button className="counter__button" onClick={() => dispatch(increment())}>
+      <button className="counter__button" onClick={increment}>
         +
       </button>
     </div>
   );
 };
 
-export default Counter;
+const mapState = state => {
+  return {
+    counter: state,
+  };
+};
+
+const mapDispatch = {
+  increment: counterActions.increment,
+  decrement: counterActions.decrement,
+  reset: counterActions.reset,
+};
+
+const connector = connect(mapState, mapDispatch); // HOC
+
+export default connector(Counter);
